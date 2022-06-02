cc5f21b0ce51ff6fef16b66ce7aae57ad8cfe007
tbskgm0 <tsubasa.kogoma@axia-works.com>
2022-06-02 23:37:22 +0900

    R.swiftのチュートリアル

--- R-tutorial/ContentView.swift
+++ R-tutorial/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isShow = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            VStack {
+                Button("フォントを変更する", action: {
+                    isShow.toggle()
+                })
+                
+                if isShow {
+                    Text("Noto Serif JP Black").font(.custom(R.font.notoSerifJPBlack.fontName, size: 15))
+                } else {
+                    Text("Large title").font(.largeTitle)
+                }
+                
+                Image(R.image.thumbnail_landmark_byodoin_hououzou.name)
+                Image(R.image.tsutenkaku_osaka_red.name)
+            }
+        }
     }
 }
 
