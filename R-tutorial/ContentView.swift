e233af856b3c7546357a74f4e43d8fab04b20afb
tbskgm0 <tsubasa.kogoma@axia-works.com>
2022-06-02 22:33:22 +0900

    Initial Commit

---
+++ R-tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  R-tutorial
+//
+//  Created by tsubasa.kogoma on 2022/06/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ R-tutorial/R_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_tutorialApp.swift
+//  R-tutorial
+//
+//  Created by tsubasa.kogoma on 2022/06/02.
+//
+
+import SwiftUI
+
+@main
+struct R_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
